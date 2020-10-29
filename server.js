f4b620b8950c99bfe4003af85c0ca6e71064d8d5
Rishi N Desai <rishindesai@yahoo.com>
2020-10-30 04:21:46 +0530

    create tables and establish relations

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+const fs = require('fs').promises
+
+const init = async() => {
+    try{
+        let queries = await fs.readFile('./db/schema.sql', 'utf-8')
+        queries = queries.replace(/\r|\n/g, '').split(";")
+        queries.pop()
+        for(query of queries){
+            db.query(query, err => {
+                if(err){
+                    console.log(err)
+                    return;
+                }
+            })
+        }
+    }
+    catch(error){
+        console.log(error)
+    }
+}
 
 const port = 3002
-app.listen(port, () => {
-    console.log("Server running on port 3002")
+init().then(() => {
+    app.listen(port, () => {
+        console.log("Server running on port 3002")
+    })
 })
